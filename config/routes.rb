7eadc275b4dbda927ba1095b9dada57f8a6d16eb
Cédric Kossi <lionel.kossi@gmail.com>
2022-09-18 18:09:33 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # namespace :v1 do
+  #   resources :people, only: [:index, :show] do
+  #     resources :pets, only: [:index, :show]
+  #   end
+  # end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'owners', to: 'v1/people#index'
+  get 'owners/:id', to: 'v1/people#show'
+
+  get 'owners_with_pets', to: 'v1/people#full_info'
+  get 'owners_with_pets/:id', to: 'v1/people#full_info_owner'
+
+  get 'pets', to: 'v1/pets#index'
+  get 'pets/:id', to: 'v1/pets#show'
 end

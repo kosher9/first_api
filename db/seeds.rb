1f823b916d7bb4f688452312d1b77fe8aa2e085d
Cédric Kossi <lionel.kossi@gmail.com>
2022-09-18 18:09:12 +0000

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+people = Person.create([{ name: 'Mat' }, {name: 'Charles'}])
+Pet.create([{ person: people[0], name: 'Roxie' }, { person: people[0], name: 'Tiger' }, { person: people[1], name: 'Otis' },{ person: people[1], name: 'Max' },{ person: people[1], name: 'Loki' }])
